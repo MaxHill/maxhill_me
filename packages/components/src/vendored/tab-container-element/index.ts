9ea32003ab0cee9a8954e653e4f7cfce349c86d0
Max Hill <max@hills.nu>
2025-10-27 22:27:59 +0100

    feat: fit-text

--- packages/components/src/vendored/tab-container-element/index.ts
+++ packages/components/src/vendored/tab-container-element/index.ts
@@ -1,6 +1,4 @@
 export { TabContainerElement, TabContainerChangeEvent } from './tab-container-element';
 import { TabContainerElement } from './tab-container-element';
 
-TabContainerElement.define();
-
 export default TabContainerElement;
