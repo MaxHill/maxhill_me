3572dc5878f33b42d7f45953cf038c929b47302e
Max Hill <max@hills.nu>
2025-11-22 23:10:27 +0100

    wip: working on form elements

--- packages/components/src/m-input/index.ts
+++ packages/components/src/m-input/index.ts
@@ -1,21 +1,14 @@
-import { MElement } from "../utils/m-element";
+import { MFormAssociatedElement } from "../utils/m-form-associated-element";
 import { query } from "../utils/query";
-import { BindAttribute } from "../utils/reflect-attribute";
 import styles from "./index.css?inline";
 
 const baseStyleSheet = new CSSStyleSheet();
 baseStyleSheet.replaceSync(styles);
 
-// TODO: formResetCallback()
-// TODO: formDisabledCallback(disabled: boolean)
-// TODO: formStateRestoreCallback(state: string, _mode: 'restore' | 'autocomplete')
-// TODO: formAssociatedCallback(_form: HTMLFormElement | null)
-
 // Text input specific
 // TODO: Selection range
 // TODO: setRangeText
-// TODO: focus the input on error
-
+// TODO: bind value to input.value to make defaultValue work
 
 
 /**
@@ -32,45 +25,16 @@ baseStyleSheet.replaceSync(styles);
  * 
  * @fires m-input-change - Fired when the example changes (detail: { example: string })
  */
-export class MInput extends MElement {
+export class MInput extends MFormAssociatedElement {
     static tagName = 'm-input';
-    static formAssociated = true;
-    static observedAttributes = ['required', 'value', 'disabled', 'label'];
+
+    static get observedAttributes() {
+        return [...super.observedAttributes]
+    }
 
     private _shadowRoot: ShadowRoot;
-    private internals: ElementInternals;
 
-    defaultValue: string = '';
-    private _value: string = '';
-    get value() {
-        return this._value
-    }
-    set value(value) {
-        // Don't set value if the input is disabled
-        if (this.disabled) return;
-        this._value = value;
-        this.internals.setFormValue(value)
-    }
-
-    @BindAttribute()
-    label = null;
-
-    @BindAttribute()
-    disabled = false;
-
-    //  ------------------------------------------------------------------------
-    //  Constraint validation                                                                     
-    //  ------------------------------------------------------------------------ 
-    private hasInteracted = false;
-    @BindAttribute()
-    required = false;
-
-
-
-
-    //  ------------------------------------------------------------------------
-    // INPUT SPECIFIC
-    @query('input')
+@query('input')
     private inputElement!: HTMLInputElement;
 
     @query('label')
@@ -79,46 +43,27 @@ export class MInput extends MElement {
     select() { this.inputElement?.select(); }
     focus(options?: FocusOptions) { this.inputElement?.focus(options); }
     blur() { this.inputElement?.blur(); }
-    // INPUT SPECIFIC
-    //  ------------------------------------------------------------------------
 
     constructor() {
         super();
         this._shadowRoot = this.attachShadow({
             mode: 'open',
-            //  ------------------------------------------------------------------------
-            // INPUT SPECIFIC
             delegatesFocus: true
         });
         this._shadowRoot.adoptedStyleSheets = [baseStyleSheet];
-        // END INPUT SPECIFIC
-        //  ------------------------------------------------------------------------
-        this.internals = this.attachInternals();
-        this.defaultValue = this.value;
     }
 
     connectedCallback() {
         this.render();
         this.firstUpdate();
 
-        //  ------------------------------------------------------------------------
-        // INPUT SPECIFIC
-
         this.inputElement.addEventListener("input", this.handleInput);
         this.inputElement.addEventListener("blur", this.handleBlur);
-        this.addEventListener("invalid", this.handleInvalid);
-        // END INPUT SPECIFIC
-        //  ------------------------------------------------------------------------
     }
 
     disconnectedCallback() {
-        //  ------------------------------------------------------------------------
-        // INPUT SPECIFIC
         this.inputElement.removeEventListener("input", this.handleInput);
         this.inputElement.removeEventListener("blur", this.handleBlur);
-        this.removeEventListener("invalid", this.handleInvalid);
-        // END INPUT SPECIFIC
-        //  ------------------------------------------------------------------------
     }
 
     attributeChangedCallback(name: string, oldValue: unknown, newValue: unknown) {
@@ -127,8 +72,7 @@ export class MInput extends MElement {
         if (name === "label") {
             this.internals.ariaLabel = newValue as string;
         }
-        //  ------------------------------------------------------------------------
-        // INPUT SPECIFIC
+
         if (name === "disabled") {
             if (this.disabled) {
                 this.inputElement.setAttribute("disabled", "");
@@ -138,23 +82,14 @@ export class MInput extends MElement {
         }
 
         if (name === "label") {
-            this.labelElement.textContent = this.label;
+            this.labelElement.textContent = this.label || "";
         }
-        // END INPUT SPECIFIC
-        //  ------------------------------------------------------------------------
     }
 
 
     //  ------------------------------------------------------------------------
     //  Event handlers                                                                     
     //  ------------------------------------------------------------------------ 
-    handleInvalid = (e: Event) => {
-        // Don't show native ui
-        e.preventDefault();
-        this.hasInteracted = true;
-        this.updateValidity();
-        this.focus();
-    }
     handleBlur = (_e: Event) => {
         this.hasInteracted = true;
         this.updateValidity();
@@ -163,45 +98,27 @@ export class MInput extends MElement {
     handleInput = (e: Event) => {
         const target = e.target as HTMLInputElement;
         if (target) { this.value = target.value; }
-        // this.hasInteracted = true;
         this.updateValidity();
     }
 
+    
     //  ------------------------------------------------------------------------
     //  Form association                                                                     
     //  ------------------------------------------------------------------------ 
-    private firstUpdate() {
-        this.internals.setFormValue(this.value);
-        this.updateValidity();
-    }
-
     formResetCallback() {
-        this.value = this.defaultValue;
-        this.hasInteracted = false;
-        this.setCustomStates();
-    }
-
-    formDisabledCallback(disabled: boolean) {
-        this.disabled = disabled;
-    }
-
-    formStateRestoreCallback(state: string, _mode: 'restore' | 'autocomplete') {
-        this.value = state;
+        super.formResetCallback();
+        // TODO: maybe we should link 
+        // value and input value in a more central way.
+        // This is kind of case by case right here.
+        // Somehow this could be done on the setter of value?
+        this.inputElement.value = this.value as string;
     }
 
     //  ------------------------------------------------------------------------
     //  Validation                                                                     
     //  ------------------------------------------------------------------------ 
-    private setCustomStates() {
-        const isValid = this.internals.validity.valid;
-        this.setState('invalid', !isValid);
-        this.setState('valid', isValid);
-        this.setState('user-invalid', !isValid && this.hasInteracted);
-        this.setState('user-valid', isValid && this.hasInteracted);
-    }
-
     // This should be unimplemented in parent class
-    private updateValidity() {
+    protected updateValidity() {
         const { value } = this;
         if (value === '' && this.required) {
             this.internals.setValidity({
@@ -212,14 +129,6 @@ export class MInput extends MElement {
         }
 
         this.setCustomStates();
-    }
-
-    //  ------------------------------------------------------------------------
-    //  Helpers                                                                     
-    //  ------------------------------------------------------------------------ 
-    private setState(name: string, condition: boolean) {
-        if (condition) { this.internals.states.add(name) }
-        else { this.internals.states.delete(name) }
     }
 
     private render() {
