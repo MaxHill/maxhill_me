78b38ed10cc591c597feee7da04f3da3170c84c8
Max Hill <max@hills.nu>
2025-11-24 16:32:48 +0100

    feat: working on validation

--- packages/components/vite.config.js
+++ packages/components/vite.config.js
@@ -3,29 +3,29 @@ import { resolve } from 'path';
 import { glob } from 'glob';
 
 const entries = Object.fromEntries(
-  glob.sync('src/**/index.ts')
-    .concat('src/register-all.ts')
-    .map(file => {
-      const entry = file.replace('src/', '').replace('/index.ts', '').replace('.ts', '');
-      return [entry, resolve(__dirname, file)];
-    })
+    glob.sync('src/**/index.ts')
+        .concat('src/register-all.ts')
+        .map(file => {
+            const entry = file.replace('src/', '').replace('/index.ts', '').replace('.ts', '');
+            return [entry, resolve(__dirname, file)];
+        })
 );
 
 const isWatchMode = process.argv.includes('--watch');
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: entries,
-      formats: ['es']
-    },
-    outDir: 'dist',
-    emptyOutDir: !isWatchMode,
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
+    build: {
+        lib: {
+            entry: entries,
+            formats: ['es']
+        },
+        outDir: 'dist',
+        emptyOutDir: !isWatchMode,
+        rollupOptions: {
+            output: {
+                entryFileNames: '[name].js',
+                assetFileNames: '[name].[ext]'
+            }
+        }
     }
-  }
 });
