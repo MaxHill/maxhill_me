d97bcd649bb448bc5128a8670bf84a988dbeea50
Max Hill <max@hills.nu>
2025-12-16 14:36:06 +0100

    chore: cleanup
    Former-commit-id: d8bcef93e5c356d34b83069a87a74af4abbf71c1

--- apps/lf-experiment/build/tasks/utils.go
+++ apps/lf-experiment/build/tasks/utils.go
@@ -3,6 +3,7 @@ package tasks
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"time"
@@ -19,11 +20,13 @@ type BuildTask interface {
 }
 
 func PollPaths(paths []string, callback func()) {
+	Assert(len(paths) < 100, "Too many paths to watch. Got %d, Max 100", len(paths))
 	// Initialize last modification times for all paths
 	lastModTimes := make(map[string]time.Time)
 	for _, path := range paths {
 		lastModTimes[path] = getLatestModTime(path)
 	}
+	Assert(len(lastModTimes) == len(paths), "LastModTimes length (%d) does not match paths to poll (%d)", len(lastModTimes), len(paths))
 
 	fmt.Println("Watching paths:", paths)
 
@@ -33,7 +36,8 @@ func PollPaths(paths []string, callback func()) {
 
 	for range ticker.C {
 		changed := false
-		for _, path := range paths {
+		for i, path := range paths {
+			Assert(i < 100, "Too files watched. Itteration %d, Max 100", i)
 			currentModTime := getLatestModTime(path)
 			if currentModTime.After(lastModTimes[path]) {
 				fmt.Printf("%s changed, triggering rebuild...\n", path)
@@ -48,11 +52,13 @@ func PollPaths(paths []string, callback func()) {
 }
 
 func getLatestModTime(dir string) time.Time {
+	Assert(dir != "", "Path cannot be empty")
+
 	var latest time.Time
 
 	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			return nil
+			return err
 		}
 		if !info.IsDir() && info.ModTime().After(latest) {
 			latest = info.ModTime()
@@ -60,6 +66,7 @@ func getLatestModTime(dir string) time.Time {
 		return nil
 	})
 
+	Assert(latest.Before(time.Now()), "LastModTime must be in the past: %s", dir)
 	return latest
 }
 
@@ -92,4 +99,10 @@ func CopyFile(src, dest string) error {
 
 	_, err = io.Copy(destFile, sourceFile)
 	return err
+}
+
+func Assert(condition bool, format string, args ...interface{}) {
+	if !condition {
+		log.Fatalf(format, args...)
+	}
 }
