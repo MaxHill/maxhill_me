ffa7e4c2ff50d6703a848c899052cb7ade9345b2
Max Hill <max@hills.nu>
2025-12-19 14:51:21 +0100

    style: trying it on

--- apps/lf-experiment/src/main.ts
+++ apps/lf-experiment/src/main.ts
@@ -2,4 +2,4 @@ import { registerAll } from "@maxhill/components/register-all";
 registerAll();
 
 // Entry point for client-side scripts and web components
-console.log("Hello from lf-experiment");
+console.log("Hello from lf-experiment!");
