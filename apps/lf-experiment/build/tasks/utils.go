4de80aba0bd8c31cd2ee15193a9fb44b9616d211
Max Hill <max@hills.nu>
2025-12-19 14:48:28 +0100

    refactor: remove live-reload
    Former-commit-id: 0471a32a3803b87425403393bca60251e66175d1

--- apps/lf-experiment/build/tasks/utils.go
+++ apps/lf-experiment/build/tasks/utils.go
@@ -1,12 +1,10 @@
 package tasks
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
-	"time"
 )
 
 type BuildResult struct {
@@ -16,39 +14,6 @@ type BuildResult struct {
 
 type BuildTask interface {
 	Build() BuildResult
-}
-
-func getLatestModTime(dir string) time.Time {
-	Assert(dir != "", "Path cannot be empty")
-
-	var latest time.Time
-
-	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() && info.ModTime().After(latest) {
-			latest = info.ModTime()
-		}
-		return nil
-	})
-
-	Assert(latest.Before(time.Now()), "LastModTime must be in the past: %s", dir)
-	return latest
-}
-
-// formatBytes converts bytes to human-readable format
-func FormatBytes(bytes int64) string {
-	const unit = 1024
-	if bytes < unit {
-		return fmt.Sprintf("%d B", bytes)
-	}
-	div, exp := int64(unit), 0
-	for n := bytes / unit; n >= unit; n /= unit {
-		div *= unit
-		exp++
-	}
-	return fmt.Sprintf("%.1f %cB", float64(bytes)/float64(div), "KMGTPE"[exp])
 }
 
 func CopyFile(src, dest string) error {
