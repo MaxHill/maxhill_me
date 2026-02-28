ab87aed194e2e9293508674b76031009c9b643bb
Max Hill <max@hills.nu>
2026-02-28 21:43:39 +0100

    feat: refactor example

--- apps/lf-experiment/src/main.ts
+++ apps/lf-experiment/src/main.ts
@@ -2,6 +2,44 @@ import { registerAll } from "@maxhill/components/register-all";
 import { CRDTDatabase } from "@maxhill/idb-distribute";
 registerAll();
 
+export class Counter
+{
+    private db: CRDTDatabase;
+    constructor(db: CRDTDatabase) {
+        this.db = db;
+    } 
+
+        async getCount(): number  {
+            const count = await this.db.get("count", "example");
+            console.log("getCount", count);
+            if (count && count.counted_value && count.counted_value > 0) {
+                return count.counted_value;
+            }
+            return 0;
+        }
+
+    async increment(): number {
+        const count = await this.getCount();
+        const new_count =  count + 1;
+        console.log(count, new_count);
+        await this.setCount(new_count);
+
+        return new_count;
+    }
+
+    async decrement(): number {
+        const count = await this.getCount();
+        const new_count = count - 1; 
+        await this.setCount(new_count);
+
+        return new_count;
+    }
+
+    private async setCount(new_count: number) {
+        await this.db.setRow("count", "example", { counted_value: new_count });
+    }
+}
+
 async function main() {
   const db = new CRDTDatabase("user::testdb", [
     {
@@ -16,30 +54,28 @@ async function main() {
     },
   ], "http://localhost:3001/sync");
   await db.open();
-  const count = await db.get("count", "example");
+  const counter = new Counter(db);
 
-  render(count?.counted_value || 0);
-  console.log("Count: ", count);
+  const count = await counter.getCount();
+  render(count);
 
   const incrementButton = document.getElementById("increment");
   incrementButton?.addEventListener("click", async (_) => {
-    const count = await db.get("count", "example");
-    await db.setRow("count", "example", { value: count?.value ? count.counted_value + 1 : 1 });
-    render(count?.counted_value + 1 || 0);
+    const count = await counter.increment();
+    render(count);
   });
 
   const decrementButton = document.getElementById("decrement");
   decrementButton?.addEventListener("click", async (_) => {
-    const count = await db.get("count", "example");
-    await db.setRow("count", "example", { value: count?.value ? count.value - 1 : 0 });
-    render(count?.value + 1 || 0);
+    const count = await counter.decrement();
+    render(count);
   });
 
 
   setInterval(async () => {
     await db.sync();
-    const count = await db.get("count", "example");
-    render(count?.counted_value || 0);
+    const count = await counter.getCount();
+    render(count);
   }, 10_000);
 }
 
