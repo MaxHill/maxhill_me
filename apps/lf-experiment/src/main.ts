7d6af6009b0974f43e9491350591fce002dba179
Max Hill <max@hills.nu>
2026-03-19 21:57:42 +0100

    feat: changes forms to be 1 column

--- apps/lf-experiment/src/main.ts
+++ apps/lf-experiment/src/main.ts
@@ -1,86 +1,83 @@
 import { registerAll } from "@maxhill/components/register-all";
-import { CRDTDatabase } from "@maxhill/idb-distribute";
+import { CRDTDatabase, Table, newDatabase } from "@maxhill/idb-distribute";
 registerAll();
 
-export class Counter {
-    private db: CRDTDatabase;
-    constructor(db: CRDTDatabase) {
-        this.db = db;
+type ShotType = {
+    club: string,
+    name: string,
+    description: string,
+}
+
+export class ShotTypeRepository {
+    table: Table;
+    constructor(private db: CRDTDatabase) {
+        this.table = this.db.table("shot_types");
     }
 
-    async getCount(): Promise<number> {
-        const count = await this.db.get("count", "example");
-        console.log("getCount", count);
-        if (count && count.counted_value && count.counted_value > 0) {
-            return count.counted_value;
-        }
-        return 0;
-    }
-
-    async increment(): Promise<number> {
-        const count = await this.getCount();
-        const new_count = count + 1;
-        console.log(count, new_count);
-        await this.setCount(new_count);
-
-        return new_count;
-    }
-
-    async decrement(): Promise<number> {
-        const count = await this.getCount();
-        const new_count = count - 1;
-        await this.setCount(new_count);
-
-        return new_count;
-    }
-
-    private async setCount(new_count: number) {
-        await this.db.setRow("count", "example", { counted_value: new_count });
+    async addShotType(shot_type: ShotType) {
+        await this.table.setRow(crypto.randomUUID(), shot_type);
     }
 }
 
 async function main() {
-    const db = new CRDTDatabase("user::testdb", {
-        tableName: "count",
-        indexes: {
-            byCount: ["example"],
-            byUser2: ["user"]
+    const db = await newDatabase("user::testdb")
+        .addTable("shot_types", {})
+        .build()
+        .open()
+
+    const shot_type_repository = new ShotTypeRepository(db);
+
+
+    await render(shot_type_repository);
+
+    const add_shot_type_form = document.getElementById("add-shot-type-form") as HTMLFormElement;
+    if (!add_shot_type_form) throw new Error("No form to add shot on page");
+
+    add_shot_type_form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const formData = new FormData(add_shot_type_form);
+
+        const name = formData.get("name")?.toString();
+        const club = formData.get("club")?.toString();
+        const description = formData.get("description")?.toString();
+
+        if (!name || !club || !description) {
+            return;
         }
-    }, "http://localhost:3001/sync");
-    await db.open();
-    const counter = new Counter(db);
 
-    const count = await counter.getCount();
-    render(count);
-
-    const incrementButton = document.getElementById("increment");
-    incrementButton?.addEventListener("click", async (_) => {
-        const count = await counter.increment();
-        render(count);
-    });
-
-    const decrementButton = document.getElementById("decrement");
-    decrementButton?.addEventListener("click", async (_) => {
-        const count = await counter.decrement();
-        render(count);
-    });
+        await shot_type_repository.addShotType({
+            name, club, description
+        });
 
 
-    setInterval(async () => {
-        await db.sync();
-        const count = await counter.getCount();
-        render(count);
-    }, 10_000);
+        await render(shot_type_repository);
+
+        add_shot_type_form.reset()
+    })
 }
 
-function render(count: number) {
-    const valueElement = document.getElementById("value");
-    if (valueElement) {
-        valueElement.textContent = String(count);
+async function render(shot_type_repository: ShotTypeRepository) {
+    const shots_container = document.getElementById("shots");
+    if (!shots_container) return;
+    shots_container.innerHTML = "";
+
+    const template = document.getElementById("shot-list-item-template");
+    if (!template) return;
+
+    for await (const shot_type of shot_type_repository.table.query()) {
+        const clone = document.importNode(template.content, true);
+
+        const name = clone.querySelector(".name");
+        if (name) name.textContent = shot_type.name;
+
+        const club = clone.querySelector(".club");
+        if (club) club.textContent = shot_type.club;
+
+        const description = clone.querySelector(".description");
+        if (description) description.textContent = shot_type.description;
+
+        shots_container?.appendChild(clone);
     }
 }
-
-// Entry point for client-side scripts and web components
-console.log("Hello from lf-experiment!");
 
 main();
