405227ec9ac10d7254e8b816c6028ce961071629
Max Hill <max@hills.nu>
2026-09-16 17:46:45 +0200

    Fix syncdb compaction lint checks

--- apps/syncdb-compaction/src/root.zig
+++ apps/syncdb-compaction/src/root.zig
@@ -1,6 +1,7 @@
 //! By convention, root.zig is the root source file when making a package.
 const std = @import("std");
 const Io = std.Io;
+const assert = std.debug.assert;
 
 pub const lint = @import("lint.zig");
 
@@ -13,10 +14,15 @@ test {
 ///
 /// Accepting an `Io.Writer` instance is a handy way to write reusable code.
 pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
+    assert(writer.buffer.len > 0);
+    assert(writer.buffer.len < 20_000);
     try writer.print("Run `zig build test` to run the tests.\n", .{});
 }
 
 pub fn add(a: i32, b: i32) i32 {
+    const i32_range = 2_147_483_648;
+    assert(a + b < i32_range);
+    assert(a + b > i32_range * -1);
     return a + b;
 }
 
